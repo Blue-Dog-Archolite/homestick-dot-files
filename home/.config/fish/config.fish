8183bc04cde20d9adf35a6598dca35dd5ad61f5b
Robert R Meyer <Robert.Meyer@huntresslabs.com>
2023-06-14 12:04:00 -0600

    Do it

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,13 +1,20 @@
-case "$-" in
-    *i*)        ;;
-    *)   return ;;
-esac
+if status is-interactive
+
+		# Setting PATH for Python 3.10
+		# The original version is saved in /Users/rmeyer/.config/fish/config.fish.pysave
+		set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+		direnv hook fish | source
+end
+
+
 
 set -x PATH "/opt/homebrew/bin" "$PATH"
 
 set -g fish_user_paths "#(brew --prefix)" $fish_user_paths
 
 set -x EDITOR nvim
+set -g EDITOR nvim
 
 # Fish hook for direnv
 # source "$HOME/.asdf/asdf.fish"
@@ -25,10 +32,4 @@ set -x EDITOR nvim
   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 # end
-
-# Setting PATH for Python 3.10
-# The original version is saved in /Users/rmeyer/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
-
-direnv hook fish | source
 
