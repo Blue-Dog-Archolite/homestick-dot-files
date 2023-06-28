d13ba47a80990243bd84dd9a86b9aea021ae35d5
Robert R Meyer <Robert.Meyer@huntresslabs.com>
2023-06-28 11:26:12 -0600

    Line updates

--- home/init.lua
+++ home/init.lua
@@ -80,8 +80,10 @@ require('packer').startup(function(use)
   -- Vim indent
   use 'preservim/vim-indent-guides'
 
+
 	-- Indent colorized
   use 'luochen1990/rainbow'
+	use "lukas-reineke/indent-blankline.nvim"
 
   -- Colors
   use 'altercation/solarized'
@@ -177,10 +179,45 @@ require('packer').startup(function(use)
   }
 end)
 
-require'nvim-tresitter.configs'.setup{
-	matchup = {
-		enable = true,
-	}
+
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    show_current_context = true,
+    show_current_context_start = true,
+}
+
+vim.opt.termguicolors = true
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+
+}
+
+require'nvim-treesitter.configs'.setup {
+  -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    -- disable = { "c", "rust" },  -- list of language that will be disabled
+  },
 }
 
 
@@ -436,14 +473,14 @@ vim.cmd("set spell")
 
 
 -- Fuck Me here we go again
-local lspconfig = require('lspconfig')
-local lsp_defaults = lspconfig.util.default_config
+-- local lspconfig = require('lspconfig')
+-- local lsp_defaults = lspconfig.util.default_config
 
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
+-- lsp_defaults.capabilities = vim.tbl_deep_extend(
+--   'force',
+--   lsp_defaults.capabilities,
+--   require('cmp_nvim_lsp').default_capabilities()
+-- )
 
 
 -- NERDTree
